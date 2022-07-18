c97d54f24419ad167863301a643549204f05f9da
Jhon Barrantes <jbarrantes@board.pe>
2022-07-18 00:38:36 -0500

    Feat: install packages on final

--- src/final-project/src/services/AxiosService.js
+++ src/final-project/src/services/AxiosService.js
@@ -13,26 +13,21 @@ export const loginUser = async({ email, password }) => {
     )
 }
 
-// TODO: Obtain All Users
 export const allUsers = async() => {
     return axios(process.env.REACT_APP_REQ_RES).get('/api/users')
 }
-// TODO: Obtain Paged Users
 export const allPagedUsers = async(page) => {
     return axios(process.env.REACT_APP_REQ_RES).get(`/api/users?page=${page}`)
 }
-// TODO: Obtain User by ID
 export const userById = async(id) => {
     return axios(process.env.REACT_APP_REQ_RES).get(`/api/users/${id}`)
 }
-// TODO: Create User
 export const createUser = async({name, job}) => {
     return axios(process.env.REACT_APP_REQ_RES).post('/api/users', {
         name,
         job
     })
 }
-// TODO: Update User
 export const updateUser = async({name, job, id}) => {
     return axios(process.env.REACT_APP_REQ_RES).put(`/api/users/${id}`, {
         name,
